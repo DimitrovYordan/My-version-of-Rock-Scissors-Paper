fa5da5e3b6186a9a2ef306aa3f423b5d161c28ff
DimitrovYordan <y.dimitrov84@abv.bg>
2022-06-03 10:42:57 +0300

    Make a few changes after feedback.
    Signed-off-by: DimitrovYordan <y.dimitrov84@abv.bg>

--- Program.cs
+++ Program.cs
@@ -6,142 +6,134 @@ namespace RockScissorsPaper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Do you wanna play with me? Write yes or no.");
-            string player = Console.ReadLine();
-
             int countPlayer = 0;
             int countComputer = 0;
             int countWrongNumber = 0;
             Random rnd = new Random();
 
+            Console.WriteLine("Ok, let's play!");
+            Console.WriteLine("Press 1 for Rock, 2 for Scissors and 3 for Paper!");
+            Console.WriteLine("Do you understand the rules mother fucker! Type yes or no.");
+            string player = Console.ReadLine();
+
             if (player == "yes")
             {
-                Console.WriteLine("Ok, let's play!");
-                Console.WriteLine("Press 1 for Rock, 2 for Scissors and 3 for Paper!");
-                Console.WriteLine("Do you understand the rules mother fucker!");
-                player = Console.ReadLine();
-
-                if (player == "yes")
+                Console.WriteLine("------------------------------------------------------");
+                while (countWrongNumber != 2)
                 {
-                    Console.WriteLine("------------------------------------------------------");
-                    while (countWrongNumber != 2)
+                    player = Console.ReadLine();
+                    int playerChoise;
+                    bool num = int.TryParse(player, out playerChoise);
+                    if (player == "end")
                     {
-                        int playerChoose = int.Parse(Console.ReadLine());
-
-                        if (playerChoose == 1)
+                        break;
+                    }
+                    else if (playerChoise == 1)
+                    {
+                        Console.WriteLine("You choose ROCK.");
+                        int computer = rnd.Next(1, 3);
+                        if (computer == playerChoise)
                         {
-                            Console.WriteLine("You choose ROCK.");
-                            int computer = rnd.Next(1, 3);
-                            if (computer == playerChoose)
-                            {
-                                Console.WriteLine("We both choose ROCK.");
-                                Console.WriteLine("No one get point, let's play again.");
-                            }
-                            else if (computer == 2)
-                            {
-                                Console.WriteLine("I choose SCISSORS.");
-                                Console.WriteLine("You win this time.");
-                                Console.WriteLine("Let's play again.");
-                                countPlayer++;
-                            }
-                            else
-                            {
-                                Console.WriteLine("I choose PAPER.");
-                                Console.WriteLine("I win this time.");
-                                Console.WriteLine("Let's play again.");
-                                countComputer++;
-                            }
-
-                            Console.WriteLine("------------------------------------------------------");
+                            Console.WriteLine("We both choose ROCK.");
+                            Console.WriteLine("No one get point, let's play again.");
                         }
-                        else if (playerChoose == 2)
+                        else if (computer == 2)
                         {
-                            Console.WriteLine("You choose SCISSORS.");
-                            int computer = rnd.Next(1, 3);
-                            if (computer == 1)
-                            {
-                                Console.WriteLine("I choose ROCK.");
-                                Console.WriteLine("I win this time.");
-                                Console.WriteLine("Let's play again.");
-                                countComputer++;
-                            }
-                            else if (computer == playerChoose)
-                            {
-                                Console.WriteLine("We both choose SCISSORS.");
-                                Console.WriteLine("No one get point, let's play again.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("I choose PAPER.");
-                                Console.WriteLine("You win this time.");
-                                Console.WriteLine("Let's play again.");
-                                countPlayer++;
-                            }
-
-                            Console.WriteLine("------------------------------------------------------");
+                            Console.WriteLine("I choose SCISSORS.");
+                            Console.WriteLine("You win this time.");
+                            Console.WriteLine("Let's play again.");
+                            countPlayer++;
                         }
-                        else if (playerChoose == 3)
+                        else
                         {
-                            Console.WriteLine("You choose PAPER.");
-                            int computer = rnd.Next(1, 3);
-                            if (computer == 1)
-                            {
-                                Console.WriteLine("I choose ROCK.");
-                                Console.WriteLine("You win this time.");
-                                Console.WriteLine("Let's play again.");
-                                countPlayer++;
-                            }
-                            else if (computer == 2)
-                            {
-                                Console.WriteLine("I choose SCISSORS.");
-                                Console.WriteLine("I win this time.");
-                                Console.WriteLine("Let's play again.");
-                                countComputer++;
-                            }
-                            else
-                            {
-                                Console.WriteLine("We both choose PAPER.");
-                                Console.WriteLine("No one get point, let's play again.");
-                            }
-
-                            Console.WriteLine("------------------------------------------------------");
+                            Console.WriteLine("I choose PAPER.");
+                            Console.WriteLine("I win this time.");
+                            Console.WriteLine("Let's play again.");
+                            countComputer++;
                         }
 
-                        if (playerChoose > 3 || playerChoose < 0)
+                        Console.WriteLine("------------------------------------------------------");
+                    }
+                    else if (playerChoise == 2)
+                    {
+                        Console.WriteLine("You choose SCISSORS.");
+                        int computer = rnd.Next(1, 3);
+                        if (computer == 1)
                         {
-                            countWrongNumber++;
-                            if (countWrongNumber == 1)
-                            {
-                                Console.WriteLine("You stupid ASS you choose a wrong number. If you do that again I will end the game.");
-                            }
-                            else if (countWrongNumber == 2)
-                            {
-                                Console.WriteLine("You stupid ASS you choose 2 times wrong number, that's mean I end the game.");
-                                break;
-                            }
+                            Console.WriteLine("I choose ROCK.");
+                            Console.WriteLine("I win this time.");
+                            Console.WriteLine("Let's play again.");
+                            countComputer++;
+                        }
+                        else if (computer == playerChoise)
+                        {
+                            Console.WriteLine("We both choose SCISSORS.");
+                            Console.WriteLine("No one get point, let's play again.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("I choose PAPER.");
+                            Console.WriteLine("You win this time.");
+                            Console.WriteLine("Let's play again.");
+                            countPlayer++;
+                        }
 
+                        Console.WriteLine("------------------------------------------------------");
+                    }
+                    else if (playerChoise == 3)
+                    {
+                        Console.WriteLine("You choose PAPER.");
+                        int computer = rnd.Next(1, 3);
+                        if (computer == 1)
+                        {
+                            Console.WriteLine("I choose ROCK.");
+                            Console.WriteLine("You win this time.");
+                            Console.WriteLine("Let's play again.");
+                            countPlayer++;
+                        }
+                        else if (computer == 2)
+                        {
+                            Console.WriteLine("I choose SCISSORS.");
+                            Console.WriteLine("I win this time.");
+                            Console.WriteLine("Let's play again.");
+                            countComputer++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("We both choose PAPER.");
+                            Console.WriteLine("No one get point, let's play again.");
+                        }
+
+                        Console.WriteLine("------------------------------------------------------");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{player} what the fuck that means?");
+                    }
+
+                    if (playerChoise > 3 || playerChoise < 0)
+                    {
+                        countWrongNumber++;
+                        if (countWrongNumber == 1)
+                        {
+                            Console.WriteLine("You stupid ASS you choose a wrong number. If you do that again I will end the game.");
+                        }
+                        else if (countWrongNumber == 2)
+                        {
+                            Console.WriteLine("You stupid ASS you choose 2 times wrong number, that's mean I end the game.");
+                            break;
                         }
 
                     }
-                }
-                else if (player == "no")
-                {
-                    Console.WriteLine("HA HA HA LOSER");
-                }
-                else
-                {
-                    Console.WriteLine($"{player} what the fuck that means?");
-                }
 
+                }
             }
             else if (player == "no")
             {
-                Console.WriteLine("Bye");
+                Console.WriteLine("HA HA HA LOSER");
             }
-            else
-            {
-                Console.WriteLine($"{player} what the fuck that means?");
-            }
+
+
 
             if (countPlayer != 0 || countComputer != 0)
             {
